acac86a4ff8a5e9688d42aafea01e762bf1de5b4
Sam <samtaktreba@gmail.com>
2023-11-23 14:45:55 +0200

    add some

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,16 @@
 import MainPage from "../pagesPreview/mainPage/MainPage";
+import { HeroWrapper, PageName, ArrowRight, ArrowLeft } from "./Hero.styled";
+import FrameRight from "../img/FrameRight.svg";
+import FrameLeft from "../img/FrameLeft.svg";
 const Hero = () => {
   return (
     <>
-      <div>
+      <HeroWrapper>
+        <PageName>*головна сторінка</PageName>
+        <ArrowRight src={FrameRight} alt="arrow right" />
+        <ArrowLeft src={FrameLeft} alt="arrow left" />
         <MainPage />
-      </div>
+      </HeroWrapper>
     </>
   );
 };
